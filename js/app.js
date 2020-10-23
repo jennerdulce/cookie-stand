43fe37ba779287cbd35192c5543e3b2bb8bb2ecb
Jenner Dulce <jennerdulce@gmail.com>
2020-10-22 22:25:46 -0700

    edited html, css, and javascript

--- js/app.js
+++ js/app.js
@@ -2,37 +2,45 @@
 
 // referenced source: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 
+
+// Jenner ~ upload image
 var homePage = document.getElementById('top');
 var img = document.createElement('img');
 img.setAttribute('src', 'img/salmon.png');
 img.setAttribute('alt', 'picture of logo of product');
 homePage.appendChild(img);
 
+// Jenner ~ global variables
 var times = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
 var parentElement = document.getElementById('main-table');
 var thead = document.createElement('thead');
 var tbody = document.createElement('tbody');
 var tfoot = document.createElement('tfoot');
 var listOfStores = [];
+var allLocationsHourly = [];
+var grandTotal = 0;
 
+// Jenner ~ random number generator
 function randomNumber(min, max) {
   return Math.floor(Math.random() * (max - min) + min);
 }
 
+// Jenner ~ render locations
 function renderAllLocations() {
   for (var i = 0; i < listOfStores.length; i++) {
     listOfStores[i].render();
   }
 }
 
-function firstRow() {
+// Jenner ~ creater first row; consists of hours
+function renderFirstRow() {
   // create thead
   var tr = document.createElement('tr');
   var th = document.createElement('th');
   th.textContent = '';
   tr.appendChild(th);
 
-  // create th and append to thead
+  // Jenner ~ create th and append to thead
   for (var i = 0; i < times.length; i++) {
     th = document.createElement('th');
     th.textContent = times[i];
@@ -47,15 +55,41 @@ function firstRow() {
 
 }
 
-function lastRow() {
+//
+function renderLastRow() {
+
+  // total cell
   var tr = document.createElement('tr');
   var td = document.createElement('td');
   td.textContent = 'Total';
+
   tr.appendChild(td);
   tfoot.appendChild(tr);
-  parentElement.appendChild(tfoot)
+  
+  // Jenner ~ hourly totals (all locations)
+  for (var i = 0; i < times.length; i++){
+    var sales = 0;
+    for (var j = 0; j < listOfStores.length; j++){
+      sales += listOfStores[j].dailyReport[i];
+    }
+    allLocationsHourly.push(sales);
+    grandTotal += sales;
+  }
+
+  for (var i = 0; i < allLocationsHourly.length; i++) {
+    td = document.createElement('td');
+    td.textContent = allLocationsHourly[i];
+    tr.appendChild(td)
+  }
+
+  td = document.createElement('td');
+  td.textContent = grandTotal;
+  tr.appendChild(td);
+  tfoot.appendChild(tr);
+  parentElement.appendChild(tfoot);
 }
 
+// Jenner ~ object constructor
 function CookieShop(location, min, max, avgSales) {
   this.location = location;
   this.min = min;
@@ -66,29 +100,31 @@ function CookieShop(location, min, max, avgSales) {
   listOfStores.push(this);
 }
 
-// gets random number of customers
+// Jenner ~ gets random number of customers
 CookieShop.prototype.getCustomers = function () {
   var hourlyCustomers = randomNumber(this.min, this.max);
   return hourlyCustomers;
 }
 
-// multiply random customer per average cookie
+// Jenner ~ multiply random customer per average cookie
 CookieShop.prototype.getHourlySales = function () {
   var hourly = Math.ceil(this.avgSales * this.getCustomers());
   this.dailyReport.push(hourly);
   return hourly;
 }
 
-// loops through hourly sales and adds cookies
+// Jenner ~ loops through hourly sales and adds cookies
 CookieShop.prototype.getDailySales = function () {
   for (var i = 0; i < times.length; i++) {
     this.totalCookies += this.getHourlySales();
   }
 }
 
+// Jenner ~ render
 CookieShop.prototype.render = function () {
   this.getDailySales();
-  // title
+  // Jenner ~ store title
+
   var tr = document.createElement('tr');
   var td = document.createElement('td');
   td.textContent = this.location;
@@ -106,7 +142,6 @@ CookieShop.prototype.render = function () {
   tr.appendChild(td);
   tbody.appendChild(tr);
   parentElement.appendChild(tbody);
-
 }
 
 new CookieShop('Seattle', 23, 65, 6.3);
@@ -115,7 +150,6 @@ new CookieShop('Dubai', 11, 38, 2.7);
 new CookieShop('Paris', 20, 38, 2.3);
 new CookieShop('Lima', 2, 16, 4.6);
 
-firstRow();
+renderFirstRow();
 renderAllLocations();
-lastRow();
-
+renderLastRow();
